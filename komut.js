5e697e791e57484763c50c7f270ed1a7979f1655
Belantus <52702095+belantus19@users.noreply.github.com>
2021-05-27 15:00:01 +0300

    Update komut.js

--- komut.js
+++ komut.js
@@ -3,19 +3,20 @@ module.exports = {
 	name:'rehber',
 	description:'rehber çağırmaya yarar',
 	execute(client,message,args){
+         if message.channel.type == 'dm' return message.reply('Adamım Bu Komutu Dm de kullanmazsın')
 	 var logkanali = 'MESAJINGİDECEĞİKANALID'
 	 var rehberid = 'REHBERROLÜID'
 	 var embed = new Discord.MessageEmbed()
-     .setColor("GREEN")
-     .setTitle("Birisi rehber çağırdı!")
-     .setFooter("Made by Belantus")
-     .setTimestamp()
-     .addField(`Çağıran kişi :`,`${message.author.username}`)
-     .addField(`Mesaj linki :`,`https://discord.com/channels/${message.guild.id}/${message.channel.id}`)
-     var log = message.guild.channels.cache.get(logkanali)
-     message.channel.send(new Discord.MessageEmbed().setDescription('✅ Başarılı bir şekilde rehber çağırıldı, en kısa sürede gelicektir lütfen bekleyiniz.'))
-     log.send(`<@&${rehberid}>`)
-     log.send(embed)
+         .setColor("GREEN")
+         .setTitle("Birisi rehber çağırdı!")
+         .setFooter("Made by Belantus")
+         .setTimestamp()
+         .addField(`Çağıran kişi :`,`${message.author.username}`)
+         .addField(`Mesaj linki :`,`https://discord.com/channels/${message.guild.id}/${message.channel.id}`)
+         var log = message.guild.channels.cache.get(logkanali)
+         message.channel.send(new Discord.MessageEmbed().setDescription('✅ Başarılı bir şekilde rehber çağırıldı, en kısa sürede gelicektir lütfen bekleyiniz.'))
+         log.send(`<@&${rehberid}>`)
+         log.send(embed)
 	}
 }
 //Made by Belantus#3519
